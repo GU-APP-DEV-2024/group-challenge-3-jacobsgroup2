4f4046a284f10da00f86c0d121cd52bbcb62b358
JSimons13 <rumham4@gmail.com>
2024-10-03 16:13:41 -0700

    Group challenge 3

--- app/src/main/java/memorygame/Board.kt
+++ app/src/main/java/memorygame/Board.kt
@@ -3,16 +3,35 @@ package memorygame
 class Board( ) {
     //Constructor to create board of different sizes.
     // Assign
-    private val cards: Array<Card> = arrayOf<Card>(Card('c', "a"),
-        Card('h', "10"),
-        Card('h', "10"),
-        Card('d', "3"),
+
+    private var cards: Array<Card> = Array<Card>(6) {_ -> Card(' ', "")}
+    private val deck: Array<Card> = arrayOf<Card> (
         Card('c', "a"),
-        Card('d', "3"))
+        Card('h', "10"),
+        Card('d', "3")
+    )
+
+    init{
+        for(i in 0.. 2) {
+            cards[2*i] = deck.random()
+            cards[(2*i)+1] = cards[2*i]
+
+
+
+           // var choseCard = deck.random()
+           // var index =
+           // cards[i] = choseCard
+        }
+        cards.shuffle()
+
+
+
+    }
 
     fun getValueAt(index:Int): Card {
         return cards[index]
     }
+
 
     data class Card(val suit: Char, val rank: String) {
 
